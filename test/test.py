6d85ecd2163ec3c9e16c421b16dc9ddd3a6f311b
peter910820 <peter910820@gmail.com>
2023-03-05 03:26:52 +0800

    delete test

--- test/test.py
+++
@@ -1,7 +0,0 @@
-import discord
-import os, re
-from pytube import YouTube, Playlist
-
-yt = YouTube('https://music.youtube.com/watch?v=M2cckDmNLMI&list=RDCLAK5uy_mRcc2Y3l-RoZsDt27qu8CBGpKt-5w7v8g')
-print('download...')
-yt.streams.filter().get_audio_only().download(filename='b.mp3')
