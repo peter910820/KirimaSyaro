5f7f19de5e6f79ba77f0bb982f38f515bab651d0
peter910820 <peter910820@gmail.com>
2024-10-06 22:38:26 +0800

    on_voice_state_update

--- cogs/general_operation.py
+++ cogs/general_operation.py
@@ -1,13 +1,35 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 class GeneralOperation(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.send("GeneralOperation cog test success!")
+    @app_commands.command(name= "test", description="testing bot delay")
+    async def test(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"delay time: {str(round(self.bot.latency*1000, 2))}ms.")  
+
+    @app_commands.command(name= "join", description= "🌟加入語音頻道🌟")
+    async def join(self, interaction: discord.Interaction) -> None:
+        if interaction.user.voice == None:
+            await interaction.response.send_message("未加入頻道")
+        elif self.bot.voice_clients == []:
+            voiceChannel = interaction.user.voice.channel
+            await voiceChannel.connect()
+        else:
+            await interaction.response.send_message("已加入頻道")
+
+    @app_commands.command(name= "leave", description= "🌟離開語音頻道🌟")
+    async def leave(self, interaction: discord.Interaction) -> None:
+        if self.bot.voice_clients != []:
+            await self.bot.voice_clients[0].disconnect()
+            self.play_queue = []
+            await self.bot.change_presence(activity = discord.Game("ブルーアーカイブ -Blue Archive-"), status=discord.Status.online)
+            await interaction.response.send_message("已離開頻道❌")
+        else:
+            await interaction.response.send_message("目前沒有在任何頻道❌")
+
 
 async def setup(bot):
     await bot.add_cog(GeneralOperation(bot))
--- main.py
+++ main.py
@@ -9,32 +9,45 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="\\", intents=intents) # \
+class SyaroBot(commands.Bot):
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user} is online.')
-    # print('logging in as {0.user}'.format(bot))
-    print(f'delay time: {str(round(bot.latency*1000, 2))}ms.')
-    game = discord.Game("ブルーアーカイブ -Blue Archive-") # status
-    await bot.change_presence(status=discord.Status.online, activity=game) # change status to game
+    def __init__(self):
+        super().__init__(
+            command_prefix = "\\",
+            intents = intents
+        )
 
-@bot.event
-async def on_command_error(ctx, error):
-    print(error)
+    async def on_ready(self):
+        print(f'{bot.user} is online.')
+        # print('logging in as {0.user}'.format(bot))
+        print(f'delay time: {str(round(bot.latency*1000, 2))}ms.')
+        game = discord.Game("ブルーアーカイブ -Blue Archive-") # status
+        await bot.change_presence(status=discord.Status.online, activity=game) # change status to game
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'延遲時間為: {str(round(bot.latency*1000, 2))}ms')
+    async def on_command_error(ctx, error):
+        print(error)
 
-@bot.command()
-async def exit(ctx):
-    await ctx.send(f'正在關閉機器人...')
-    await bot.close()
+    async def on_voice_state_update(self, member, before, after):
+        # 當使用者開始講話（綠燈亮起）
+        if after.self_mute is False and after.self_deaf is False and before.self_mute is True:
+            await member.send(f"{member.guild.name}正在講話")
+            print(f"{member.name} is now talking!")
+        
+        # 當使用者停止講話（綠燈熄滅）
+        elif after.self_mute is True and before.self_mute is False:
+            print(f"{member.name}不講話了")
 
-async def main():
-    await bot.load_extension('cogs.general_operation')
-    await bot.load_extension('cogs.youtube_play')
-    await bot.start(os.getenv('TOKEN'))
+    async def ping(ctx):
+        await ctx.send(f'延遲時間為: {str(round(bot.latency*1000, 2))}ms')
 
-asyncio.run(main())
+    async def exit(ctx):
+        await ctx.send(f'正在關閉機器人...')
+        await bot.close()
+
+    async def setup_hook(self):
+        await bot.load_extension('cogs.general_operation')
+        # await bot.load_extension('cogs.youtube_play')
+        await bot.tree.sync(guild = None)
+
+bot = SyaroBot()
+bot.run(os.getenv("TOKEN"))
